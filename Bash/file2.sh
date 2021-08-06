80a2e11d050768bc4f5135c1e44d1014bac7b046
soumik606 <57164086+soumik606@users.noreply.github.com>
2021-08-06 11:58:08 +0530

    Update file2.sh

--- Bash/file2.sh
+++ Bash/file2.sh
@@ -3,11 +3,12 @@ sum_eve=0
 sum_odd=0
 for i in ${input[@]}
 do
-	if [(($i % 2)) -eq  0]
-	then
-		sum_eve = $sum_eve + $i
-	else
-		sum_odd = $sum_odd + $i
-	fi
+        if !(($i % 2));
+        then
+                ((sum_eve = sum_eve + i))
+        else
+                ((sum_odd = sum_odd + i))
+        fi
 done
-echo "$sum_eve - $sum_odd"
+((diff = sum_eve-sum_odd))
+echo "$sum_eve - $sum_odd = " $diff
